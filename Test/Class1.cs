4e96e0914945ba6f47379ffcf1ea90bcad92a999
David Dassau <todieahero@hotmail.com>
2018-03-03 09:46:20 -0600

    initial setup

---
+++ Test/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StringCalculator;
+using NUnit.Framework;
+using StringCalculator;
+
+namespace Test
+{
+    [TestFixure]
+    public class Class1
+    {
+    }
+}
